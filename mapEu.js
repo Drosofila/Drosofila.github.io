636df27683c547b4aec90dfbc61ecfb785a3eeec
Drosofila <guilhermeoliveira306@gmail.com>
2022-06-22 13:57:31 -0300

    teste mapa novo

--- mapEu.js
+++ mapEu.js
@@ -1,8 +1,8 @@
 var Map = L.map('map').setView([58.0, 5.0], 3.4);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
     maxZoom: 19,
-    attribution: '© OpenStreetMap'
+    attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
 }).addTo(Map);
 
 var polygon1 = L.polygon([

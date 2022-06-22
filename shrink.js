d4b2348571f9f7407f9c75285b806d74760aa420
Drosofila <guilhermeoliveira306@gmail.com>
2022-06-22 00:48:33 -0300

    header animation 1.14

--- shrink.js
+++ shrink.js
@@ -25,10 +25,10 @@ function scrollFunction() {
 
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("ul1").style.margin = "10px 200px 0 0"
-    document.getElementById("header").style.fontSize = "10pt";
+    document.getElementById("ul1").style.fontSize = "10pt";
   } else {
     document.getElementById("ul1").style.margin = "30px 0 0 30px";
-    document.getElementById("header").style.fontSize = "16pt";
+    document.getElementById("ul1").style.fontSize = "16pt";
   }
 
 }
